e1b09bbaa812369b61fde6021027a97950d38070
anvarbekyoldoshboyev <smagroup@gmail.com>
2026-06-25 00:31:16 +0500

    .

---
+++ DjangoTemplate/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Todo
+# Create your views here.
+
+
+def home(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos
+    }
+    return render(request, 'index.html', context=context)
